d3cbb6e77e246d26d634d44757297743c3e2ea3c
tanikura <122531271+tanikura@users.noreply.github.com>
2023-03-09 12:59:52 +0000

    [Add] headerレイアウト作成中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
+  get "home/about" => "home#about", as: "about"
+  
   resources :books, only: [:new, :index, :show, :destroy]
   resources :users, only: [:show, :edit]
 end
