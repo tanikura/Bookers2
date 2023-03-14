3a0167616e13e265bb8aae61d4bfebe32c4c4d84
tanikura <122531271+tanikura@users.noreply.github.com>
2023-03-14 14:42:10 +0000

    [Add]aboutページのレイアウトまで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,34 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      redirect_to book_path
+    else
+      render :new
+    end
+  end
+  
   def index
+    @books = Book.page(params[:page]).reberse_order
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to book_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:image, :caption)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
-  get "home/about" => "home#about", as: "about"
-  
+  get "homes/about" => "homes#about", as: "about"
+
   resources :books, only: [:new, :index, :show, :destroy]
   resources :users, only: [:show, :edit]
 end
